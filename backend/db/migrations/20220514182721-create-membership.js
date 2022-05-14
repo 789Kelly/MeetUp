b2fc123a40715f9ec0bfbd0683493ab9562a2331
Kelly <kelly.a.artola@ricealumni.net>
2022-05-14 13:59:53 -0500

    created Membership model and migration

---
+++ backend/db/migrations/20220514182721-create-membership.js
@@ -0,0 +1,39 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Memberships", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+        },
+      },
+      groupId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Groups",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Memberships");
+  },
+};
