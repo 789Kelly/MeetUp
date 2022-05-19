38ad040cf90b53b802e9fecb383b9f0b6fdb1741
Kelly <kelly.a.artola@ricealumni.net>
2022-05-18 20:20:25 -0500

    made group seeders

---
+++ backend/db/seeders/20220519005745-groups.js
@@ -0,0 +1,101 @@
+"use strict";
+
+const { Group } = require("../models");
+
+const groups = [
+  {
+    name: "Group One",
+    about: "test",
+    type: "test",
+    private: true,
+    city: "Azua",
+    state: "Dominican Republic",
+  },
+  {
+    name: "Group Two",
+    about: "test",
+    type: "test",
+    private: false,
+    city: "Chiguiri Arriba",
+    state: "Panama",
+  },
+  {
+    name: "Group Three",
+    about: "test",
+    type: "test",
+    private: true,
+    city: "La Union",
+    state: "El Salvador",
+  },
+  {
+    name: "Group Four",
+    about: "test",
+    type: "test",
+    private: false,
+    city: "Moscow",
+    state: "Russia",
+  },
+  {
+    name: "Group Five",
+    about: "test",
+    type: "test",
+    private: true,
+    city: "Brussels",
+    state: "Belgium",
+  },
+  {
+    name: "Group Six",
+    about: "test",
+    type: "test",
+    private: true,
+    city: "Xian",
+    state: "China",
+  },
+  {
+    name: "Group Seven",
+    about: "test",
+    type: "test",
+    private: true,
+    city: "Florence",
+    state: "Italy",
+  },
+  {
+    name: "Group Eight",
+    about: "test",
+    type: "test",
+    private: true,
+    city: "Ankara",
+    state: "Turkey",
+  },
+  {
+    name: "Group Nine",
+    about: "test",
+    type: "test",
+    private: true,
+    city: "Cancun",
+    state: "Mexico",
+  },
+];
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
