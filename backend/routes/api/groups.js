f5482f8500a197b444166fbf9fe445e7beee7f0d
Kelly <kelly.a.artola@ricealumni.net>
2022-05-30 23:06:15 -0500

    trying out a validator

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -64,9 +64,11 @@ const validateEvent = [
     .exists({ checkFalsy: true })
     .withMessage("Description is required"),
   check("startDate").isAfter().withMessage("Start date must be in the future"),
-  check("endDate")
-    .isAfter(toDate("startDate"))
-    .withMessage("End date is less than start date"),
+  check("endDate").custom((value, { req }) => {
+    if (value < req.body.startDate) {
+      throw new Error("Message");
+    }
+  }),
   handleValidationErrors,
 ];
 
